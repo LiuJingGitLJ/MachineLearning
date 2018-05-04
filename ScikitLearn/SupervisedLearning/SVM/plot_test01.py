94d1c70bf59c10beb4b1e24903257c47a6f684be
liujing <15201078137@163.com>
2018-05-04 18:27:49 +0800

    1
    1

--- ScikitLearn/SupervisedLearning/SVM/plot_test01.py
+++ ScikitLearn/SupervisedLearning/SVM/plot_test01.py
@@ -21,38 +21,25 @@ Reshape your data either using array.reshape(-1, 1) if your data has a single fe
 print (clf.predict([[0, 1]]))   #测试数据
 
 print("########################################\n")
-
 import numpy as np
-
 import pylab as pl
 
-
-
 #生成随机点数据集
-
 np.random.seed(0) #固定随机值
-
 x = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
-
 y = [0] *20 +[1] * 20
 
 print(x)
-
 print(y)
 
-
-
 clf2 = svm.SVC(kernel='linear')
-
 clf2.fit(x, y)
-
 print(clf2.support_)
 #画出散点图
 #画出支持向量的点，参数：x，y，大小
 pl.scatter(clf2.support_vectors_[:,0],clf2.support_vectors_[:,1],s=80)
 #画出全部的点，参数：x，y，颜色，colormap，形状
 pl.scatter(x[:,0],x[:,1],c=y,cmap=pl.cm.Paired,marker='o')
-
 pl.axis('tight')
 #pl.savefig("dd") 保存绘图
 pl.show()
